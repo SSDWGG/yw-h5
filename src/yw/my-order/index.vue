9fcc7617d399fe18a56b3bed7f1bcf1e06e211de
wgg <1982549567@qq.com>
2025-02-26 08:58:19 +0800

    fix

--- src/yw/my-order/index.vue
+++ src/yw/my-order/index.vue
@@ -134,23 +134,24 @@ export default {
         "getBrandWCPayRequest",
         {
           // 公众号名称，由商户传入
-          appId: data.appid,
+          appId: data.appId,
           // 时间戳，自1970年以来的秒数
-          timeStamp: Math.floor(Date.now() / 1000),
+          timeStamp:data.timeStamp,
           // 随机串
           nonceStr: data.nonceStr,
-          package: 'prepay_id='+data.prepayId,
+          package: data.packageValue,
           // 微信签名方式：
-          signType: "RSA",
+          signType: data.signType,
           // 微信签名
-          paySign: data.sign,
+          paySign: data.paySign,
         },
         function (res) {
+          console.log('支付完成',res,that.storeOrderId);
           if (res.err_msg == "get_brand_wcpay_request:ok") {
             // 使用以上方式判断前端返回,微信团队郑重提示：
             // res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-            console.log('支付完成',this.storeOrderId);
-            uni.navigateTo({ url: '/yw/pay-result/index?storeOrderId='+this.storeOrderId })
+            console.log('支付完成',that.storeOrderId);
+            uni.navigateTo({ url: '/yw/pay-result/index?storeOrderId='+that.storeOrderId })
 
           }
         }
@@ -169,6 +170,7 @@ export default {
       })
     },
     handleBuy(storeOrderId) {
+      let that = this
       if ((/micromessenger/.test(navigator.userAgent.toLowerCase()))) {
             // 带着orderId跳转到支付页逻辑
             console.log('微信浏览器');
--- src/yw/submit/index.vue
+++ src/yw/submit/index.vue
@@ -153,23 +153,26 @@ export default {
   methods: {
     // data为网页端接口请求参数列表
     onBridgeReady(data) {
+      console.log(666,data);
+
       let that = this
       WeixinJSBridge.invoke(
         "getBrandWCPayRequest",
         {
           // 公众号名称，由商户传入
-          appId: data.appid,
+          appId: data.appId,
           // 时间戳，自1970年以来的秒数
-          timeStamp: Math.floor(Date.now() / 1000),
+          timeStamp:data.timeStamp,
           // 随机串
           nonceStr: data.nonceStr,
-          package: 'prepay_id='+data.prepayId,
+          package: data.packageValue,
           // 微信签名方式：
-          signType: "RSA",
+          signType: data.signType,
           // 微信签名
-          paySign: data.sign,
+          paySign: data.paySign,
         },
         function (res) {
+          console.log('支付完成',res,that.storeOrderId);
           if (res.err_msg == "get_brand_wcpay_request:ok") {
             // 使用以上方式判断前端返回,微信团队郑重提示：
             // res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
@@ -199,7 +202,6 @@ export default {
             console.log('微信浏览器');
             this.storeOrderId = res.data.storeOrderId
             jsapiPayOrder(res.data.storeOrderId).then(e => {
-              console.log(e.data);
               if (typeof WeixinJSBridge == "undefined") {
                 if (document.addEventListener) {
                   document.addEventListener("WeixinJSBridgeReady", onBridgeReady, false);
